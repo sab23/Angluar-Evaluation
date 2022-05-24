5098f1f551c8cbbec08cd1b0ede317fab2bb07fe
sab23 <67158755+sab23@users.noreply.github.com>
2022-05-24 14:17:20 +0200

    Exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularcliproject';
+  statistique1: Statistique = new Statistique("1", "Harry Potter", "9 000 000 000");
+  statistique2: Statistique = new Statistique("2", "Seigneur des Anneaux", "3 000 000 000");
 }
--- src/app/models/statistique.ts
+++ src/app/models/statistique.ts
@@ -1,11 +1,11 @@
-export classe Statistique = {
+export class Statistique {
     identifiant?: string;
     nom?: string;
-    valeur?: string;
+    chiffre?: string;
 
-    constructor(identifiant: string, nom: string, valeur: string){
+    constructor(identifiant: string, nom: string, chiffre: string){
         this.identifiant = identifiant;
         this.nom = nom;
-        this.valeur = valeur;
+        this.chiffre = chiffre;
     }
 }
