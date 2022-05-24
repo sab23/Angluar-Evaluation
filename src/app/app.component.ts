d019c4218e8f51fae82008f9b02411b00db49bec
sab23 <67158755+sab23@users.noreply.github.com>
2022-05-24 14:44:42 +0200

    Exercice 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,15 @@ import { Statistique } from './models/statistique';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularcliproject';
-  statistique1: Statistique = new Statistique("1", "Harry Potter", "9 000 000 000");
-  statistique2: Statistique = new Statistique("2", "Seigneur des Anneaux", "3 000 000 000");
+  title = 'Statistiques franchises';
+  statistiques: Statistique[] = [
+    new Statistique("1",  "Harry Potter", "9 000 000 000"),
+    new Statistique("2", "Seigneur des Anneaux", "3 000 000 000")];
+
+    constructor(){
+      setTimeout(() => {
+        this.statistiques.push(new Statistique("3", "Star Wars", "4 000 000 000"));
+      }, 5000);
+    }
+
 }
